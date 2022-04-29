4b1ebcc051ce0b3fdf63ca0f0808f7cee74aac7e
Double VV <narongbunyon@gmail.com>
2022-04-29 11:19:49 +0700

    #14 create form add question

--- models/item_model.js
+++ models/item_model.js
@@ -45,6 +45,5 @@ module.exports=
 {
     getQuestions,
     createQuestion,
-    removeQuestion
 }
 
--- public/views/edit/edit.js
+++ public/views/edit/edit.js
@@ -1,3 +1,17 @@
+const URL = "http://localhost:3000/api/items";
+
+function getQuestions() {
+    axios.get(URL).then(items=>{
+        console.log(items.data);
+    })
+
+}
+
+
+// add question
+const add = document.querySelector(".add")
+add.addEventListener("click",getQuestions);
+
 let create_quiz=document.querySelector(".create_quiz");
 // start code here -----------------------
 create_quiz.style.display="none";
@@ -12,8 +26,21 @@ let showCreate = document.querySelector(".buutonShow");
 showCreate.addEventListener("click",displayQuiz);
 
 
-const URL = ""
 
-function getURL() {
-    alert("The URL of this page is: " + window.location.href);
-}
+// let bigdiv =document.createElement("div");
+// bigdiv.className="question";
+// bigdiv.appendChild(divtitle);
+
+// let divtitle = document.createElement("div");
+// divtitle.className="title_quiz";
+// divtitle.appendChild(divQuiz);
+
+
+
+// let divQuiz = document.createElement("div");
+// divQuiz.className="name_quiz";
+
+
+// let divAnswer = document.createElement("anwer");
+
+// console.log(bigdiv);
